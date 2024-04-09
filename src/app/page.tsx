29ba0e059658c6b947983a1d8d36671c8eb6efe7
Nguyễn Quốc Chung <quocchung.starack@gmail.com>
2024-04-09 15:28:34 +0700

    feat: complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { StyleBoxBody } from "./style-mui";
 import { CircularProgress } from "@mui/material";
 
 export default function OverView() {
-    const router = useRouter();
-    router.push('/Overview')
+    // const router = useRouter();
+    // router.push('/Overview')
     return (
         <StyleBoxBody>
             <CircularProgress />
